98f180f5a66cd59596c612048137c98fff50afee
Saidamin Qambarov <saidaminqambarov408@gmail.com>
2022-01-23 01:54:54 +0500

    MenuButton

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -2,9 +2,10 @@ from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
 from data.text import text
+from keyboards.default.menuKeyboard import menu
 from loader import dp
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(text=text['start_text'].format(message.from_user.full_name))
+    await message.answer(text=text['start_text'].format(message.from_user.full_name), reply_markup=menu)
