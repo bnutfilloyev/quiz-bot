4970b8afec218447fa8fa7489d6dfa6bd765398e
yoshlikmedia <yoshlikmedia@gmail.com>
2022-01-23 01:41:58 +0500

    text.py bug fixed

--- data/text.py
+++ data/text.py
@@ -1,5 +1,5 @@
 text = {
-    "start_text": "Привет, {}!",
+    "start_text": "Привет, <b>{}!</b>",
     'create_quiz': "Создать викторину",
     'cancel_button': "Отмена",
     'create_quiz_hello': "Нажмите на кнопку ниже и создайте викторину! "
