78dd7383a31ac2416d543fc5d2038fcce3206262
yoshlikmedia <yoshlikmedia@gmail.com>
2022-01-23 02:04:27 +0500

    new dict add on text.py

--- utils/db_api/mongo.py
+++ utils/db_api/mongo.py
@@ -7,8 +7,8 @@ storage = MemoryStorage()
 
 database = client['Quizzer']
 user_db = database['users']
-quizzes_database = database['quizzes_database']
-quizzes_owners = database['quizzes_owners']
+quizzes_database = database['quizes_database']
+quizzes_owners = database['quizes_owners']
 
 if __name__ == "__main__":
     print(quizzes_database.find_one_and_update())
